f11cc5c72374b9d21d139ca2b5528b0506a241ac
João Antônio <joaoasouza982@gmail.com>
2023-10-01 15:42:06 -0300

    Feat: Creating base service

---
+++ FluentValidation.API/Services/BaseServices/BaseService.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation.API.Interfaces.Settings;
+
+namespace FluentValidation.API.Services.BaseServices;
+
+public abstract class BaseService<TEntity>
+    where TEntity : class
+{
+    private readonly IValidator<TEntity> _validator;
+    private readonly INotificationHandler _notificationHandler;
+
+    public BaseService(IValidator<TEntity> validator, INotificationHandler notificationHandler)
+    {
+        _validator = validator;
+        _notificationHandler = notificationHandler;
+    }
+
+    protected async Task<bool> ValidateAsync(TEntity entity)
+    {
+        var validationResult = await _validator.ValidateAsync(entity);
+
+        if (!validationResult.IsValid)
+        {
+            foreach(var error in validationResult.Errors)
+            {
+                _notificationHandler.AddNotification(error.PropertyName, error.ErrorMessage);
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+}
