7a074c97e2c3f52de507fe76e6562a8b494da9e7
João Antônio <joaoasouza982@gmail.com>
2023-10-04 11:43:14 -0300

    Feat: Cpf Validator tests

---
+++ UnitTests/SettingsTests/ValidatorsTests/CpfValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation.API.Settings.ValidationSettings;
+
+namespace UnitTests.SettingsTests.ValidatorsTests;
+public sealed class CpfValidatorTests
+{
+    [Theory]
+    [InlineData("58217437025")]
+    [InlineData("82532961007")]
+    [InlineData("45632504069")]
+    [InlineData("80804091021")]
+    [InlineData("00514934034")]
+    public void ValidateCpf_SuccessfulScenario_ReturnsTrue(string cpf)
+    {
+        var validateCpfResult = CpfValidator.ValidateCpf(cpf);
+
+        Assert.True(validateCpfResult);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("aaaa")]
+    [InlineData("joao")]
+    [InlineData("123912039")]
+    [InlineData("123")]
+    [InlineData("80904091021")]
+    public void ValidaCpf_CpfInvalid_ReturnsFalse(string cpf)
+    {
+        var validateCpfResult = CpfValidator.ValidateCpf(cpf);
+
+        Assert.False(validateCpfResult);
+    }
+}
