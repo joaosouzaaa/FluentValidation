0a83efe583ede20ba7bd6c782603a089ca6daa21
João Antônio <joaoasouza982@gmail.com>
2023-10-04 15:24:10 -0300

    Test: Person Integration tests

---
+++ IntegrationTests/PersonIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.API.Constants.RouteConstants;
+using IntegrationTests.Fixture;
+using System.Net;
+using System.Net.Http.Json;
+using TestBuilders;
+
+namespace IntegrationTests;
+public sealed class PersonIntegrationTests : HttpClientFixture
+{
+    private const string baseUrl = "api/Person/";
+
+    [Fact]
+    public async Task ValidatePersonAsync_SuccessfulScenario_Returns200OK()
+    {
+        // A
+        var person = PersonBuilder.NewObject().DomainBuild();
+
+        // A
+        var validatePersonHttpResponseMessageResult = await _httpClient.PostAsJsonAsync(baseUrl + PersonRouteConstants.ValidatePerson, person);
+
+        // A
+        Assert.Equal(validatePersonHttpResponseMessageResult.StatusCode, HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task ValidatePersonAsync_InvalidPerson_Returns400BadRequest()
+    {
+        // A
+        var person = PersonBuilder.NewObject().WithCpf("worng").DomainBuild();
+
+        // A
+        var validatePersonHttpResponseMessageResult = await _httpClient.PostAsJsonAsync(baseUrl + PersonRouteConstants.ValidatePerson, person);
+
+        // A
+        Assert.Equal(validatePersonHttpResponseMessageResult.StatusCode, HttpStatusCode.BadRequest);
+    }
+}
