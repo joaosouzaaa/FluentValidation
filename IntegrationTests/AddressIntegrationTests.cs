c85b3b311b035557d23abca2c9a37847e1c211b8
João Antônio <joaoasouza982@gmail.com>
2023-10-04 15:09:46 -0300

    Test: Address Integration Tests

---
+++ IntegrationTests/AddressIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.API.Constants.RouteConstants;
+using IntegrationTests.Fixture;
+using System.Net;
+using System.Net.Http.Json;
+using TestBuilders;
+
+namespace IntegrationTests;
+public sealed class AddressIntegrationTests : HttpClientFixture
+{
+    private const string baseUrl = "api/Address/";
+
+    [Fact]
+    public async Task ValidateAddressAsync_SuccessfulScenario_Returns200OK()
+    {
+        // A
+        var address = AddressBuilder.NewObject().DomainBuild();
+
+        // A
+        var validateAddressHttpResponseMessageResult = await _httpClient.PostAsJsonAsync(baseUrl + AddressRouteConstants.ValidateAddress, address);
+
+        // A
+        Assert.Equal(validateAddressHttpResponseMessageResult.StatusCode, HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task ValidateAddressAsync_InvalidAddres_Returns400BadRequest()
+    {
+        // A
+        var address = AddressBuilder.NewObject().WithNumber("").DomainBuild();
+
+        // A
+        var validateAddressHttpResponseMessageResult = await _httpClient.PostAsJsonAsync(baseUrl + AddressRouteConstants.ValidateAddress, address);
+
+        // A
+        Assert.Equal(validateAddressHttpResponseMessageResult.StatusCode, HttpStatusCode.BadRequest);
+    }
+}
